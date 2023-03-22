52f8f42a582f3c2a723c59679b07b0b936598cd4
PatroklosGeorgiadis <61284594+PatroklosGeorgiadis@users.noreply.github.com>
2023-03-22 21:40:55 +0200

    Spawner + Death and Game Over

---
+++ Video_Game_Main/Assets/VGame Scripts/EnemyScripts/Spawner.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float enemiesToSpawn = 5;
+    public float activeEnemies = 0;
+    public float totalEnemiesSpawned;
+    public float enemiesAtOnce = 2;
+    public float originRandomOffset = 2;
+    public UnityEvent onSpawnerEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeEnemies = 0;
+        totalEnemiesSpawned = 0;
+
+        if (enemiesToSpawn > 0) SpawnEnemy();
+    }
+
+    void SpawnEnemy()
+    {
+        activeEnemies++;
+        totalEnemiesSpawned++;
+
+        GameObject clone = GameObject.Instantiate(enemyPrefab, transform.position + RandomSpawnLocalPosition(), transform.rotation);
+
+        EnemyHealth enemyHealth = clone.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.RegisterSpawner(this);
+        }
+
+        if (activeEnemies < enemiesAtOnce)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    public void NotifyDeath(EnemyHealth enemyHealth)
+    {
+        activeEnemies--;
+
+        if (totalEnemiesSpawned < enemiesToSpawn)
+        {
+            SpawnEnemy();
+        }
+        else if (activeEnemies <= 0)
+        {
+            onSpawnerEnd.Invoke();
+        }
+    }
+
+    Vector3 RandomSpawnLocalPosition()
+    {
+        float x = Random.Range(-originRandomOffset, originRandomOffset);
+        float z = Random.Range(-originRandomOffset, originRandomOffset);
+
+        return new Vector3(x, 0, z);
+    }
+}
---
+++ Video_Game_Main/Assets/VGame Scripts/Fireball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float damage = 200f;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) return;
+
+        Health health = other.gameObject.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+
+            Destroy(gameObject);
+        }
+    }
+}
